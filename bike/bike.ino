559d98aca53460195f29c46b5ba4a37f6e753dda
urt <urt5002urt@gmail.com>
2025-05-21 10:24:51 +0200

    Add project files.

---
+++ bike/bike.ino
@@ -0,0 +1,173 @@
+#include <EncButton.h>
+#define EB_NO_COUNTER
+#define EB_NO_BUFFER
+#include <ESP8266WiFi.h>
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "Wire.h"
+#include "log.h"
+
+#define GYRO_INTERRUPT_PIN D2 // use pin 15 on ESP8266
+#define GYRO_SCL_PIN D3
+#define GYRO_SDA_PIN D1
+#define CRANKS_LOW_PIN D8
+#define CRANKS_HIGH_PIN D7
+#define WHEEL_LOW_PIN D6
+#define WHEEL_HIGH_PIN D5
+#define DERAILLEUR_PIN A0
+#define BTN_PIN D4
+
+#define SEND_TIME_MS 2000
+
+MPU6050 mpu;
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+
+float euler[3];
+
+volatile bool mpuInterrupt = false;
+void ICACHE_RAM_ATTR dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+unsigned int cranks_count = 0;
+bool cranks_side = false; //false - lastc call was low. true - last call was high
+void ICACHE_RAM_ATTR cranks_low()
+{
+    if (!cranks_side)
+    {
+        cranks_count++;
+        cranks_side = true;
+    }
+}
+void ICACHE_RAM_ATTR cranks_high()
+{
+    if (cranks_side)
+    {
+        cranks_count++;
+        cranks_side = false;
+    }
+}
+
+unsigned int wheel_count = 0;
+bool wheel_side = false; //false - last call was low. true - last call was high
+void ICACHE_RAM_ATTR wheel_low()
+{
+    if (!wheel_side)
+    {
+        wheel_count++;
+        wheel_side = true;
+    }
+}
+void ICACHE_RAM_ATTR wheel_high()
+{
+    if (wheel_side)
+    {
+        wheel_count++;
+        wheel_side = false;
+    }
+}
+
+void mpu_setup()
+{
+    Wire.begin(GYRO_SDA_PIN, GYRO_SCL_PIN);
+    Wire.setClock(400000);
+
+    LOG_MPU("Initializing I2C devices...\n");
+    mpu.initialize();
+    pinMode(GYRO_INTERRUPT_PIN, INPUT);
+
+    LOG_MPU("Testing device connections...\n");
+    LOG_MPU(mpu.testConnection() ? "MPU6050 connection successful\n" : "MPU6050 connection failed\n");
+
+    LOG_MPU("Initializing DMP...\n");
+    devStatus = mpu.dmpInitialize();
+
+    mpu.setXGyroOffset(220);
+    mpu.setYGyroOffset(76);
+    mpu.setZGyroOffset(-85);
+    mpu.setZAccelOffset(1788);
+
+    if (devStatus == 0) {
+        LOG_MPU("Enabling DMP...\n");
+        mpu.setDMPEnabled(true);
+        LOG_MPU("Enabling interrupt detection...\n");
+        attachInterrupt(GYRO_INTERRUPT_PIN, dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+        LOG_MPU("DMP ready! Waiting for first interrupt...\n");
+        dmpReady = true;
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    }
+    else
+    {
+        LOG_MPU("DMP Initialization failed (code %d)\n", devStatus);
+    }
+}
+
+void setup()
+{
+#ifdef LOG_USB_ENABLE
+    Serial.begin(115200);
+    delay(2000);
+#endif
+    mpu_setup();
+    pinMode(CRANKS_LOW_PIN, INPUT); //cant be pulled up
+    attachInterrupt(CRANKS_LOW_PIN, cranks_low, RISING);
+    pinMode(CRANKS_HIGH_PIN, INPUT_PULLUP);
+    attachInterrupt(CRANKS_HIGH_PIN, cranks_high, FALLING);
+
+    pinMode(WHEEL_LOW_PIN, INPUT_PULLUP);
+    attachInterrupt(WHEEL_LOW_PIN, wheel_low, FALLING);
+    pinMode(WHEEL_HIGH_PIN, INPUT_PULLUP);
+    attachInterrupt(WHEEL_HIGH_PIN, wheel_high, FALLING);
+    pinMode(DERAILLEUR_PIN, INPUT);
+
+    pinMode(BTN_PIN, INPUT_PULLUP);
+}
+
+void mpu_loop()
+{
+    if (!dmpReady) return;
+    if (!mpuInterrupt && fifoCount < packetSize) return;
+    mpuInterrupt = false;
+    mpuIntStatus = mpu.getIntStatus();
+    fifoCount = mpu.getFIFOCount();
+
+    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+        mpu.resetFIFO();
+    }
+    else if (mpuIntStatus & 0x02) {
+        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+        mpu.getFIFOBytes(fifoBuffer, packetSize);
+        fifoCount -= packetSize;
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
+        mpu.dmpGetEuler(euler, &q);
+    }
+}
+
+unsigned long last;
+void loop()
+{
+    mpu_loop();
+    unsigned long now = millis();
+    if ((now % SEND_TIME_MS < 10 ) && (now > last + 10))
+    {
+        Serial.printf("cranks: %d\twheel: %d\n", cranks_count/2, wheel_count/2);
+        //Serial.printf("Cranks: low %d\thigh %d\tWheel: low %d\thigh %d\n",!digitalRead(CRANKS_LOW_PIN), digitalRead(CRANKS_HIGH_PIN), digitalRead(WHEEL_LOW_PIN), digitalRead(WHEEL_HIGH_PIN));
+        cranks_count = 0;
+        wheel_count = 0;
+        last = now;
+    }
+}
---
+++ bike/log.h
@@ -0,0 +1,54 @@
+#ifndef LOG_CFG_H
+#define LOG_CFG_H
+
+#define LOG_USB_ENABLE
+
+#ifdef LOG_USB_ENABLE
+
+#define LOG_MPU_ENABLE
+#define LOG_STARTUP_ENABLE
+//#define LOG_HALL12_ENABLE
+//#define LOG_HALL3_ENABLE
+
+#endif // LOG_USB_ENABLE
+
+
+#ifdef LOG_USB_ENABLE
+#define LOG_USB_MATR(matr)						\
+{												\
+	Serial.printf("%s\n", #matr);				\
+	for (int i = 0; i < MATR_SIZE; i++)			\
+	{											\
+		for (int j = 0; j < MATR_SIZE; j++)		\
+			Serial.printf("%d ", matr[j][i]);	\
+		Serial.printf("\n");					\
+	}											\
+}
+#endif
+
+#ifdef LOG_MPU_ENABLE
+#define LOG_MPU(format, ...) Serial.printf(format, ##__VA_ARGS__)
+#else
+#define LOG_MPU(format, ...) ;
+#endif
+
+#ifdef LOG_STARTUP_ENABLE
+#define LOG_STARTUP(format, ...) Serial.printf(format, ##__VA_ARGS__)
+#else
+#define LOG_STARTUP(format, ...) ;
+#endif
+
+#ifdef LOG_HALL12_ENABLE
+#define LOG_HALL12(format, ...) Serial.printf(format, ##__VA_ARGS__)
+#else
+#define LOG_HALL12(format, ...) ;
+#endif
+
+#ifdef LOG_HALL3_ENABLE
+#define LOG_HALL3(format, ...) Serial.printf(format, ##__VA_ARGS__)
+#else
+#define LOG_HALL3(format, ...) ;
+#endif
+
+
+#endif
